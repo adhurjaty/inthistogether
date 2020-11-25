5a36aa81efde21ccc97b5cbad2548359b48ca0a3
Dan Kouba <Ductapemaster@gmail.com>
2020-11-25 14:58:29 -0800

    Add holiday form

---
+++ src/pages/niceList/index.md
@@ -0,0 +1,9 @@
+---
+templateKey: nicelist
+title: The ITTE Nice List 2020
+path: /nicelist
+seo:
+  browserTitle: ITTE Nice List 2020 | In This Together Events
+  description: ITTE Nice List
+  title: ITTE Nice List 2020 | In This Together Events
+---
---
+++ src/templates/nicelist.js
@@ -0,0 +1,79 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
+import isBefore from "date-fns/is_before";
+import endOfDay from "date-fns/end_of_day"
+import ReactMarkdown from "react-markdown";
+
+import MeetupTemplate from "./meetup";
+import Layout from "../components/Layout";
+import HTMLContent from "../components/Content";
+import "../styles/past-meetups-page.scss";
+
+export const NiceListTemplate = ({
+  title,
+  content,
+  bodyIsMarkdown = false,
+}) => {
+  return (
+    <article className="niceList">
+      <div className="container  niceList-container">
+        <h1 className="niceList-title">{title}</h1>
+        {bodyIsMarkdown ? (
+          <ReactMarkdown className="niceList-description" source={content} />
+        ) : (
+          <HTMLContent className="niceList-description" content={content} />
+        )}
+      </div>
+    </article>
+  );
+};
+
+NiceListTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  meetups: PropTypes.array,
+};
+
+const NiceList = ({ data }) => {
+  const { markdownRemark: page } = data;
+  const {
+    frontmatter: {
+      seo: { title: seoTitle, description: seoDescription, browserTitle },
+    },
+  } = page;
+
+  return (
+    <Layout footerData={data.footerData} navbarData={data.navbarData}>
+      <Helmet>
+        <meta name="title" content={seoTitle} />
+        <meta name="description" content={seoDescription} />
+        <title>{browserTitle}</title>
+      </Helmet>
+      <script type="text/javascript" src="https://form.jotform.com/jsform/203287743361053"></script>
+    </Layout>
+  );
+};
+
+NiceList.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default NiceList;
+
+export const NiceListQuery = graphql`
+  query NiceList($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        seo {
+          browserTitle
+          title
+          description
+        }
+      }
+    }
+  }
+`;
