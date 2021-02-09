73defedc5848e401d9455a0f4c97f59ef4099a4f
Dan Kouba <Ductapemaster@gmail.com>
2021-02-09 12:52:35 -0800

    Add test explorers page

---
+++ src/pages/btExplorers/index.md
@@ -0,0 +1,9 @@
+---
+templateKey: btExplorers
+title: Better Together Festival — Explorers Inspiration
+path: /BTExplorers
+seo:
+  browserTitle: Better Together Festival — Explorers Inspiration | In This Together Events
+  description: Better Together Festival — Explorers Inspiration
+  title: Better Together Festival — Explorers Inspiration | In This Together Events
+---
---
+++ src/templates/btExplorers.js
@@ -0,0 +1,80 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
+import isBefore from "date-fns/is_before";
+import endOfDay from "date-fns/end_of_day"
+import ReactMarkdown from "react-markdown";
+
+import MeetupTemplate from "./meetup";
+import Layout from "../components/Layout";
+import HTMLContent from "../components/Content";
+import "../styles/past-meetups-page.scss";
+
+export const BTExplorersTemplate = ({
+  title,
+  content,
+  bodyIsMarkdown = false,
+}) => {
+  return (
+    <article className="btExplorers">
+      <div className="container  btExplorers-container">
+        <h1 className="btExplorers-title">{title}</h1>
+        {bodyIsMarkdown ? (
+          <ReactMarkdown className="btExplorers-description" source={content} />
+        ) : (
+          <HTMLContent className="btExplorers-description" content={content} />
+        )}
+        <a data-pin-do="embedBoard" data-pin-board-width="800" data-pin-scale-height="800" data-pin-scale-width="80" href="https://www.pinterest.com/inthistogetherevents/explorers/"></a>
+      </div>
+    </article>
+  );
+};
+
+BTExplorersTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  meetups: PropTypes.array,
+};
+
+const BTExplorers = ({ data }) => {
+  const { markdownRemark: page } = data;
+  const {
+    frontmatter: {
+      seo: { title: seoTitle, description: seoDescription, browserTitle },
+    },
+  } = page;
+
+  return (
+    <Layout footerData={data.footerData} navbarData={data.navbarData}>
+      <Helmet>
+        <meta name="title" content={seoTitle} />
+        <meta name="description" content={seoDescription} />
+        <title>{browserTitle}</title>
+        <script async defer src="//assets.pinterest.com/js/pinit.js"></script>
+      </Helmet>
+    </Layout>
+  );
+};
+
+BTExplorers.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default BTExplorers;
+
+export const BTExplorersQuery = graphql`
+  query BTExplorers($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        seo {
+          browserTitle
+          title
+          description
+        }
+      }
+    }
+  }
+`;
