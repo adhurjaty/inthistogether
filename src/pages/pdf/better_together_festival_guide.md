8f1f73cea4e2814a12d1bb88710736a2f815342b
Dan Kouba <Ductapemaster@gmail.com>
2021-02-09 21:51:18 -0800

    Test festival guide page

---
+++ src/pages/pdf/better_together_festival_guide.md
@@ -0,0 +1,12 @@
+---
+templateKey: pdf-serve
+title: Better Together Festival Guide
+path: /BTFestivalGuide
+pdf: "/img/guide-small.pdf"
+pdfImg: "/img/guide-small.png"
+pdfLinkText: Download Festival Guide Here!
+seo:
+  browserTitle: Better Together Festival Guide | In This Together Events
+  description: Better Together Festival Guide
+  title: Better Together Festival Guide | In This Together Events
+---
---
+++ src/templates/pdf-serve.js
@@ -0,0 +1,79 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import ReactMarkdown from "react-markdown";
+import Helmet from "react-helmet";
+
+import Layout from "../components/Layout";
+import HTMLContent from "../components/Content";
+import "../styles/about-page.scss";
+
+export const PdfPageTemplate = props => {
+  const { page } = props;
+
+  return (
+    <article className="about">
+      <div className="about-container  container">
+        <section className="about-header">
+          <div className="about-titleWrapper">
+            <h1 className="about-title">{page.frontmatter.title}</h1>
+          </div>
+          <div className="about-imageWrapper">
+            <img src={page.frontmatter.pdfImg} />
+          </div>
+        </section>
+        <section className="section">
+          <HTMLContent className="about-description">
+              <a href={page.frontmatter.pdfImg}>{page.frontmatter.pdfLinkText}</a>
+          </HTMLContent>
+        </section>
+      </div>
+    </article>
+  );
+};
+
+const PdfPage = ({ data }) => {
+  const { markdownRemark: page, footerData, navbarData } = data;
+  const {
+    frontmatter: {
+      seo: { title: seoTitle, description: seoDescription, browserTitle },
+      pdfImg,
+      pdfLinkText,
+    },
+  } = page;
+
+  return (
+    <Layout footerData={footerData} navbarData={navbarData}>
+      <Helmet>
+        <meta name="title" content={seoTitle} />
+        <meta name="description" content={seoDescription} />
+        <title>{browserTitle}</title>
+      </Helmet>
+      <PdfPageTemplate page={{ ...page, bodyIsMarkdown: false }} />
+    </Layout>
+  );
+};
+
+PdfPage.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default PdfPage;
+
+export const pdfPageQuery = graphql`
+  query PdfPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        pdf
+        pdfImg
+        seo {
+          browserTitle
+          title
+          description
+        }
+      }
+    }
+  }
+`;
