36b864fc60eed5b5329ff61af8cb2699cc454a29
Dan Kouba <Ductapemaster@gmail.com>
2021-02-10 13:15:16 -0800

    Add google tag manager plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,33 @@ module.exports = {
         logo: "./src/img/favicon.png",
       },
     },
+    {
+        resolve: `gatsby-plugin-google-gtag`,
+        options: {
+            // You can add multiple tracking ids and a pageview event will be fired for all of them.
+            trackingIds: [
+            "G-SR60NQ9CMJ", // Google Analytics / GA
+            // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+            // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+            ],
+            // This object gets passed directly to the gtag config command
+            // This config will be shared across all trackingIds
+            gtagConfig: {
+            optimize_id: "G-SR60NQ9CMJ",
+            anonymize_ip: true,
+            cookie_expires: 0,
+            },
+            // This object is used for configuration specific to this plugin
+            pluginConfig: {
+            // Puts tracking script in the head instead of the body
+            head: false,
+            // Setting this parameter is also optional
+            respectDNT: true,
+            // Avoids sending pageview hits from custom paths
+            exclude: [],
+            },
+        },
+    },
     "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
 };
